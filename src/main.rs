9475ba1cf20d8f5a468df2207d84314f31079a21
Einar <mail@einarsundgren.se>
2019-01-05 01:21:35 +0100

    Some tests and basic functionality is there

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 extern crate rand;
 
 use rand::Rng;
+use std::{thread, time};
 
-static alive:u8  = 1;
-static dead:u8  = 0;
+static ALIVE:u8  = 1;
+static DEAD:u8  = 0;
 
 fn main() {
 
 let mut board: Vec<Vec<u8>>;
- 
- board = initiate_board(create_board(40,40), 40.0);
- 
- loop {
- match check_cell(&board, 20,20) {
-     1=> {println!("Will be alive next round.");
-     break;},
-     _=> println!("Will be dead next round."),
- }
- }
 
+let ten_millis = time::Duration::from_millis(200);
+
+
+board = create_board(10,10);
+board[4][1]=ALIVE;
+board[4][2]=ALIVE;
+board[4][3]=ALIVE;
+
+for i in 1..10 {
 print_board(&board);
+board = update_board(board);
+thread::sleep(ten_millis);
 
+}
 }
 
 #[test]
@@ -35,8 +38,6 @@ for (i, x) in board.iter_mut().enumerate() {
         //board[x as &usize][y as &usize]= 1;
     }
 }
-
-print_board(&board);
 
 for (i, x) in board.iter().enumerate() {
     for (j, mut y) in x.iter().enumerate() {
@@ -55,18 +56,57 @@ fn alive_cell_checking(){
 let mut board: Vec<Vec<u8>>;
 board = create_board(10,10);
 
-board[1][1] = alive;
-assert!(check_cell(&board, 1,1)==dead);
+let startx:usize = 1;
+let starty:usize = 1;
 
+board[startx][starty] = ALIVE;
 
-board[0][0] = 1u8;
-assert!(check_cell(&board, 1,1)==dead);
+// Cecking that 1,1 is set to live.
+assert_eq!(board[startx][starty], ALIVE);
 
-board[0][1] = 1u8;
-assert!(check_cell(&board, 1,1)==dead);
+//No other cells set. Assuming the cell dies.
+println!("**** First check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
 
-board[0][2] = 1u8;
-assert!(check_cell(&board, 1,1)==alive);
+// Setting one cell in the vincinity to live. Assuming dead cell.
+board[startx-1][starty-1] = ALIVE;
+println!("**** Second check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting two cells in the vincinity to live. Assuming living cell.
+board[startx-1][starty] = ALIVE;
+println!("**** Third check ****");
+assert!(check_cell(&board, startx,starty)==ALIVE);
+
+// Setting three cells in the vincinity to live. Assuming living cell.
+board[startx-1][starty+1] = ALIVE;
+println!("**** Forth check ****");
+assert!(check_cell(&board, startx,starty)==ALIVE);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx][starty-1] = ALIVE;
+println!("**** Fifth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx][starty+1] = ALIVE;
+println!("**** Sixth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty-1] = ALIVE;
+println!("**** Seventh check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty] = ALIVE;
+println!("**** Eight check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty+1] = ALIVE;
+println!("**** Ninth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
 
 }
 
@@ -75,21 +115,59 @@ fn dead_cell_checking(){
 let mut board: Vec<Vec<u8>>;
 board = create_board(10,10);
 
-board[1][1] = dead;
+let startx:usize = 1;
+let starty:usize = 1;
 
-assert!(check_cell(&board, 1,1)==dead);
+board[startx][starty] = DEAD;
 
-board[0][0] = alive;
-assert!(check_cell(&board, 1,1)==dead);
+// Cecking that 1,1 is set to live.
+assert_eq!(board[startx][starty], DEAD);
 
-board[0][1] = alive;
-assert!(check_cell(&board, 1,1)==alive);
+//No other cells set. Assuming the cell dies.
+println!("**** First check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
 
-board[0][2] = alive;
-//assert!(check_cell(&board, 1,1)==dead);
+// Setting one cell in the vincinity to live. Assuming dead cell.
+board[startx-1][starty-1] = ALIVE;
+println!("**** Second check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting two cells in the vincinity to live. Assuming DEAD cell.
+board[startx-1][starty] = ALIVE;
+println!("**** Third check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting three cells in the vincinity to live. Assuming living cell.
+board[startx-1][starty+1] = ALIVE;
+println!("**** Forth check ****");
+assert!(check_cell(&board, startx,starty)==ALIVE);
+
+// Setting four cells in the vincinity to live. Assuming dead cell.
+board[startx+1][starty+1] = ALIVE;
+println!("**** Fifth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx][starty+1] = ALIVE;
+println!("**** Sixth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty-1] = ALIVE;
+println!("**** Seventh check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty] = ALIVE;
+println!("**** Eight check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
+
+// Setting four cells in the vincinity to live. Assuming living dead.
+board[startx+1][starty+1] = ALIVE;
+println!("**** Ninth check ****");
+assert!(check_cell(&board, startx,starty)==DEAD);
 
 }
-
 
 
 fn create_board(w:usize, h:usize) -> Vec<Vec<u8>> { 
@@ -98,30 +176,32 @@ fn create_board(w:usize, h:usize) -> Vec<Vec<u8>> {
 }
 
 fn print_board(board:&Vec<Vec<u8>>) {
-let mut rownum: u8 = 0;
+//let mut rownum: u8 = 0;
 
-print!("___");
+
+
+for (rownum, x) in board.iter().enumerate().rev() {
+    print!("{:?} ", rownum);
+    if rownum <  10  {print!("__");}
+    if rownum >= 10 {print!("_");}
+    
+    for (colnum, y) in x.iter().enumerate() {
+        match board[rownum][colnum] {
+        1 => print!("00_"),
+        _ => print!("  _"),
+    }
+    }
+    println!("");
+    //rownum +=1;
+}
+
+print!("    ");
 for (i, item) in board.iter().enumerate() {
     print!("{:?}", i);
     if i <10  {print!("__");}
     if i >=10 {print!("_");}
 }
 println!("");
-
-for row in board.iter() {
-    print!("{:?} ", rownum);
-      if rownum <10  {print!("__");}
-    if rownum >=10 {print!("_");}
-    for val in row.iter() {
-    match val {
-    1 => print!("00_"),
-   // 2 => print!(" "),
-    _ => print!("  _"),
-    }
-    }
-    println!("");
-    rownum +=1;
-}
 }
 
 fn initiate_board(mut board: Vec<Vec<u8>>, chance_of_life:f64) -> Vec<Vec<u8>> {
@@ -141,50 +221,71 @@ fn initiate_board(mut board: Vec<Vec<u8>>, chance_of_life:f64) -> Vec<Vec<u8>> {
     return board;
 }
 
+fn update_board(board: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+
+    let mut new_board: Vec<Vec<u8>> = create_board(board.len(), board[0].len());
+    for (x, row) in board.iter().enumerate() {
+        for (y, val) in row.iter().enumerate() {
+                      new_board[x][y] = check_cell(&board,x,y);
+        }
+    }
+    
+    return new_board;
+}
+
 fn check_cell(board: &Vec<Vec<u8>>, x:usize,y:usize) -> u8{
     let mut live_neighbous: u8 = 0;
  
     if x >= board.len() || y >= board[x].len() {
-        println!("Checking item outside of board.");
-        return dead;    
+      //  println!("Checking item outside of board.");
+        return DEAD;    
     }
 
-    let is_alive: bool = board[x][y] < 0;
+    let is_alive: bool = board[x][y] != DEAD;
     
 
-    for check_x in -1i8..2 {
-        for check_y in -1i8..2 {
+    for check_x in -1i8..2i8 {
+        for check_y in -1i8..2i8 {
             
-            // println!("Checking: x:{:?}, y:{:?}", x as i8 + check_x, y as i8 + check_y);
-            if x as i8 + check_x >= 0 
+        //    println!("Checking: x:{:?}, y:{:?} Relative from start: ({:?},{:?})",  (check_x + (x as i8)) as i8, y as i8 + check_y, check_x, check_y);
+            
+            if check_x != 0i8 || check_y != 0i8 {           
+
+            if  (x as i8 + check_x >= 0) 
                 && ((x as i8 + check_x) as usize) < board.len() 
-                && y as i8 + check_y >= 0 
-                && ((y as i8 + check_y) as usize) < board[x].len() 
+                && (y as i8 + check_y >= 0) 
+                && ((y as i8 + check_y) as usize) < board[x].len()
             {
-              //  println!("Valid item, checking content.");
+          //      println!("Valid item, checking content.");
               
-                if board[(x as i8 + check_x) as usize][(y as i8 + check_y) as usize] == alive 
+                if board[(x as i8 + check_x) as usize][(y as i8 + check_y) as usize] == ALIVE 
                 {
                     live_neighbous +=1;
+            //        println!("It was alive.");
+
+                } else {
+                   // println!("It was dead.");
                 }
             } else {
-                println!("Item outside of bounds.");
+                // println!("Item invalid.");
             }
-            //println!("");            
+            // println!("Checked x: {:?} y: {:?}. Counting {:?} live neighbours.", x as i8 + check_x, y as i8 + check_y, live_neighbous);
+            // println!("");    
+            } else {
+               // println!("Centered item. Did not check.");
+            }       
         }
     }
 
     match is_alive {
         true => match live_neighbous {
-            2 | 3 =>    return alive,
-            _ =>        return dead,
+            2 | 3 =>    return ALIVE,
+            _ =>         return DEAD,
         },
 
         false => match live_neighbous {
-            3 =>        return alive,
-            _ =>        return dead,
+            3 =>        return ALIVE,
+            _ =>        return DEAD,
         },
     }
-
-
 }
